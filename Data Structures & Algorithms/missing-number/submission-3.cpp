18f37e2b720fca63b6cfc8cbc57b2ecd5b2ea31e
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-08 15:12:56 +0530

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.cpp
@@ -0,0 +1,34 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         for(int i=0;i<=n;i++){
+//             bool found=false;
+//             for(int j=0;j<n;j++){
+//                 if(nums[j]==i){
+//                     found=true;
+//                     break;
+//                 }
+//             }
+//             if(!found){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i){
+                return i;
+            }
+            
+        }
+        return n;
+    }
+};
